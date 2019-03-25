897b3b5a2b5cf48f8d02de9a51611150abc8247a
kei7 <kei950120@gmail.com>
2019-03-25 10:25:16 +0000

    change installsh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/bin/sh
 set -u
 DOT_DIR=$(cd $(dirname $0); pwd)
 cd $DOT_DIR
@@ -10,6 +10,7 @@ do
 		[[ "$f" == ".git"]] && continue
 		[[ "$f" == ".DS_Store"]] && continue
 		[[ "$f" == ".gitignore"]] && continue
+		[[ "$f" == ".gitconfig"]] && continue
 		ln -snfv ~/dotfiles/"$f" ~/
 done
 
