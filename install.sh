de2dda7a0ff865a3b601466153a499cd900c810a
kei7 <kei950120@gmail.com>
2019-03-25 10:58:48 +0000

    modify install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,15 @@ cd $DOT_DIR
 git submodule init
 git submodule update
 
-for f in .??*
-do 
-		[[ "$f" == ".git"]] && continue
-		[[ "$f" == ".DS_Store"]] && continue
-		[[ "$f" == ".gitignore"]] && continue
-		[[ "$f" == ".gitconfig"]] && continue
-		ln -snfv ~/dotfiles/"$f" ~/
-done
+#for f in .??*;
+#do 
+#		 $f == ".git" && continue
+#		 $f == ".DS_Store" && continue
+#		 $f == ".gitignore" && continue
+#		 $f == ".gitconfig" && continue
+#		ln -snfv ~/dotfiles/"$f" ~/
+#done
+ln -snfv /dotfiles/.zshrc ~/
+ln -snfv /dotfiles/.vim ~/ 
+ln -snfv /dotfiles/.vimrc ~/ 
 
